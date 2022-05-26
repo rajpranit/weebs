73f472c8cd0ca1848927421db7721e840257b887
rajpranit <rajpranit1207@gmail.com>
2022-05-26 11:59:27 +0530

    new pages

--- weebs/urls.py
+++ weebs/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('category/',views.category,name='category'),
+    path('category',views.category,name='category'),
+    path('category/anime',views.anime,name='anime'),
+    path('category/manhwa',views.manhwa,name='manhwa'),
+    path('category/manhua',views.manhua,name='manhua'),
+    path('category/doujins',views.doujins,name='doujins'),
+    path('category/hentai',views.hentai,name='hentai'),
+    path('single-audio/',views.single_audio,name='single-audio'),
+    path('single-gallery/',views.single_gallery,name='single-gallery'),
+    path('single-standard/',views.single_standard,name='single-standard'),
+    path('single-video/',views.single_video,name='single-video'),
     path('about/',views.about,name='about'),
     path('contact',views.contact,name='contact'),
 ]
--- weebs/views.py
+++ weebs/views.py
@@ -3,11 +3,43 @@ from django.shortcuts import render
 def index(request):
     return render(request,'weebs/index.html')
 
+
 def category(request):
     return render(request,'weebs/category.html')
 
+def anime(request):
+    return render(request,'weebs/anime.html')
+
+
+def manhwa(request):
+    return render(request,'weebs/manhwa.html')
+
+
+def manhua(request):
+    return render(request,'weebs/manhua.html')
+
+
+def doujins(request):
+    return render(request,'weebs/doujins.html')
+
+
+def hentai(request):
+    return render(request,'weebs/hentai.html')
+
 def about(request):
     return render(request,'weebs/about.html')
+
+def single_audio(request):
+    return render(request,'weebs/single-audio.html')
+
+def single_gallery(request):
+    return render(request,'weebs/single-gallery.html')
+
+def single_standard(request):
+    return render(request,'weebs/single-standard.html')
+
+def single_video(request):
+    return render(request,'weebs/single-video.html')
 
 def contact(request):
     return render(request,'weebs/contact.html')
